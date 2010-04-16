e4261d92e175b814d2abd7dab4978da07944cfc3
Deepesh Gujarathi <dgujarathi@nvidia.com>
2010-04-16 19:33:37 +0530

    tegra mmc: enable/diable sdio controller clock
    The sd controller clock will be switch on/off on a per transaction
    basis, this will ensure that the clock is off during OSIdle which
    should inturn help better DVFS results.
    
    fix for Bug:673699
    
    Change-Id: If479fe4aea6ae2c00818c84b43fc38a66437c643
    Reviewed-on: http://git-master/r/1242
    Reviewed-by: Janne Hellsten <jhellsten@nvidia.com>
    Tested-by: Janne Hellsten <jhellsten@nvidia.com>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -933,9 +933,11 @@ static void sdhci_set_clock(struct sdhci_host *host, unsigned int clock)
 
 	writew(0, host->ioaddr + SDHCI_CLOCK_CONTROL);
 
-	if (clock == 0)
+	if (clock == 0) {
+		if (host->ops->set_clock)
+			div = host->ops->set_clock(host, 0);
 		goto out;
-
+	}
 	host->last_clock = clock;
 
 	div = 0;
