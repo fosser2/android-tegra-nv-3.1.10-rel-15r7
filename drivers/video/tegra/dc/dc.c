4a057f18eeffb07113b072aaa8f345a728f693d7
Donghan Ryu <dryu@nvidia.com>
2011-05-22 18:30:31 +0900

    tegra: dc: adding max pixclock check for hdmi
    tegra_dc_hdmi_equal doesn't check pixclock and some devices doesn't
    support 148.5Mhz pixclock which is needed for 1080p@60. However,
    adding 1080p@30 to the supported hdmi mode array makes
    tegra_dc_hdmi_equal to retun 1080p@60. Therefore, this commit adds
    max pixclock check to distinguish modes with different pixclock
    
    Bug: 815409
    
    Change-Id: Ifbf07929e3c7a92172856518a55e9d4a04f0b943
    Reviewed-on: http://git-master/r/32511
    Reviewed-by: Niket Sirsi <nsirsi@nvidia.com>
    Tested-by: Niket Sirsi <nsirsi@nvidia.com>

--- drivers/video/tegra/dc/dc.c
+++ drivers/video/tegra/dc/dc.c
@@ -1315,7 +1315,7 @@ static void tegra_dc_set_out(struct tegra_dc *dc, struct tegra_dc_out *out)
 
 }
 
-unsigned tegra_dc_get_out_height(struct tegra_dc *dc)
+unsigned tegra_dc_get_out_height(const struct tegra_dc *dc)
 {
 	if (dc->out)
 		return dc->out->height;
@@ -1324,7 +1324,7 @@ unsigned tegra_dc_get_out_height(struct tegra_dc *dc)
 }
 EXPORT_SYMBOL(tegra_dc_get_out_height);
 
-unsigned tegra_dc_get_out_width(struct tegra_dc *dc)
+unsigned tegra_dc_get_out_width(const struct tegra_dc *dc)
 {
 	if (dc->out)
 		return dc->out->width;
@@ -1332,6 +1332,15 @@ unsigned tegra_dc_get_out_width(struct tegra_dc *dc)
 		return 0;
 }
 EXPORT_SYMBOL(tegra_dc_get_out_width);
+
+unsigned tegra_dc_get_out_max_pixclock(const struct tegra_dc *dc)
+{
+	if (dc->out && dc->out->max_pixclock)
+		return dc->out->max_pixclock;
+	else
+		return 0;
+}
+EXPORT_SYMBOL(tegra_dc_get_out_max_pixclock);
 
 static void tegra_dc_vblank(struct work_struct *work)
 {
