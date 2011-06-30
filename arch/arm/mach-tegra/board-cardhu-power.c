13f1a06da289722e2564ba533cf94fa0233f4c1a
Ray Poudrier <rapoudrier@nvidia.com>
2011-06-30 14:16:42 -0700

    arm: tegra: cardhu: Add KBC wake event
    Enables LP0 to stay asleep on PM269
    
    Bug 848662
    
    Change-Id: Ie29c9f89ee1d3a3b8f3272cdbc2714b05d5b0bbc
    Reviewed-on: http://git-master/r/39450
    Reviewed-by: Varun Colbert <vcolbert@nvidia.com>
    Tested-by: Varun Colbert <vcolbert@nvidia.com>

--- arch/arm/mach-tegra/board-cardhu-power.c
+++ arch/arm/mach-tegra/board-cardhu-power.c
@@ -959,10 +959,11 @@ static struct tegra_suspend_platform_data cardhu_suspend_data = {
 	.separate_req	= true,
 	.corereq_high	= true,
 	.sysclkreq_high	= true,
-	.wake_enb	= TEGRA_WAKE_GPIO_PV0 | TEGRA_WAKE_PWR_INT | TEGRA_WAKE_RTC_ALARM,
+	.wake_enb	= TEGRA_WAKE_KBC_EVENT | TEGRA_WAKE_GPIO_PV0 |
+			  TEGRA_WAKE_PWR_INT | TEGRA_WAKE_RTC_ALARM,
 	.wake_high	= TEGRA_WAKE_RTC_ALARM,
 	.wake_low	= TEGRA_WAKE_GPIO_PV0 | TEGRA_WAKE_PWR_INT,
-	.wake_any	= 0,
+	.wake_any	= TEGRA_WAKE_KBC_EVENT,
 	.cpu_lp2_min_residency = 2000,
 };
 
