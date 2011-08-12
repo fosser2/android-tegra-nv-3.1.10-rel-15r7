c34df94c603182403bccf7e3c00482db3c1204d1
Pradeep Goudagunta <pgoudagunta@nvidia.com>
2011-08-12 16:00:38 +0530

    serial: tegra: Add check for uart state before access
    Add proper check for uart state in public api to make sure client
    driver would not access UART registers when it is in invalid state.
    
    Bug 827693
    
    Change-Id: Icfaed824d98023206dacd0346a90e34f2fc6935e
    Reviewed-on: http://git-master/r/46923
    Tested-by: Pradeep Goudagunta <pgoudagunta@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- drivers/serial/tegra_hsuart.c
+++ drivers/serial/tegra_hsuart.c
@@ -1515,6 +1515,11 @@ void tegra_uart_set_mctrl(struct uart_port *uport, unsigned int mctrl)
 	struct tegra_uart_port *t;
 
 	t = container_of(uport, struct tegra_uart_port, uport);
+	if (t->uart_state != TEGRA_UART_OPENED) {
+		dev_err(t->uport.dev, "Uart is in invalid state\n");
+		return;
+	}
+
 	spin_lock_irqsave(&uport->lock, flags);
 	if (mctrl & TIOCM_RTS) {
 		t->rts_active = true;
