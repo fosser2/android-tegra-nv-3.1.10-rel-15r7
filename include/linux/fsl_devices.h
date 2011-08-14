8fcdb169f9cfb45bfef3fc8511c4fc4f477af8da
Suresh Mangipudi <smangipudi@nvidia.com>
2011-08-14 18:27:30 +0530

    usb: fsl_udc: setting usb charging current
    Setting the charging current limit through
    the regulator for drawing the vbus current.
    
    Bug 854993
    
    Change-Id: I4e1d3578de7f234f7fc9cd1af09fdd1d9dc227d9
    Reviewed-on: http://git-master/r/46704
    Reviewed-by: Suresh Mangipudi <smangipudi@nvidia.com>
    Tested-by: Suresh Mangipudi <smangipudi@nvidia.com>
    Reviewed-by: Hanumanth Venkateswa Moganty <vmoganty@nvidia.com>

--- include/linux/fsl_devices.h
+++ include/linux/fsl_devices.h
@@ -72,6 +72,7 @@ struct fsl_usb2_platform_data {
 	unsigned int			port_enables;
 	void				*phy_config;
 	enum fsl_usb2_phy_types usb_phy_type;
+	const char			*charge_regulator;
 };
 
 /* Flags in fsl_usb2_mph_platform_data */
