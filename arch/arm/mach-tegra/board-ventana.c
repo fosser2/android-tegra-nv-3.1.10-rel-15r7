58ee0aef0b9abb136c13ecb38fd82d3d49e55ff9
Alok Chauhan <alokc@nvidia.com>
2011-08-16 14:39:42 +0530

    arm: tegra: Updated i2c platform data
    Updated i2c platform data to add i2c arb lost recovery funtion
    and corresponding gpio numbers to i2c pins
    
    bug 854305
    
    This is cherry pick of change http://git-master/r/#change,43200
    in main but hand-merged.
    
    Change-Id: I4098a512625c16598b8596d0e46d285ca9b92d2b
    Reviewed-on: http://git-master/r/47290
    Reviewed-by: Alok Chauhan <alokc@nvidia.com>
    Tested-by: Alok Chauhan <alokc@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- arch/arm/mach-tegra/board-ventana.c
+++ arch/arm/mach-tegra/board-ventana.c
@@ -441,6 +441,9 @@ static struct tegra_i2c_platform_data ventana_i2c1_platform_data = {
 	.bus_count	= 1,
 	.bus_clk_rate	= { 400000, 0 },
 	.slave_addr = 0x00FC,
+	.scl_gpio		= {TEGRA_GPIO_PC4, 0},
+	.sda_gpio		= {TEGRA_GPIO_PC5, 0},
+	.arb_recovery = arb_lost_recovery,
 };
 
 static const struct tegra_pingroup_config i2c2_ddc = {
@@ -460,6 +463,9 @@ static struct tegra_i2c_platform_data ventana_i2c2_platform_data = {
 	.bus_mux	= { &i2c2_ddc, &i2c2_gen2 },
 	.bus_mux_len	= { 1, 1 },
 	.slave_addr = 0x00FC,
+	.scl_gpio		= {0, TEGRA_GPIO_PT5},
+	.sda_gpio		= {0, TEGRA_GPIO_PT6},
+	.arb_recovery = arb_lost_recovery,
 };
 
 static struct tegra_i2c_platform_data ventana_i2c3_platform_data = {
@@ -467,6 +473,9 @@ static struct tegra_i2c_platform_data ventana_i2c3_platform_data = {
 	.bus_count	= 1,
 	.bus_clk_rate	= { 400000, 0 },
 	.slave_addr = 0x00FC,
+	.scl_gpio		= {TEGRA_GPIO_PBB2, 0},
+	.sda_gpio		= {TEGRA_GPIO_PBB3, 0},
+	.arb_recovery = arb_lost_recovery,
 };
 
 static struct tegra_i2c_platform_data ventana_dvc_platform_data = {
@@ -474,6 +483,9 @@ static struct tegra_i2c_platform_data ventana_dvc_platform_data = {
 	.bus_count	= 1,
 	.bus_clk_rate	= { 400000, 0 },
 	.is_dvc		= true,
+	.scl_gpio		= {TEGRA_GPIO_PZ6, 0},
+	.sda_gpio		= {TEGRA_GPIO_PZ7, 0},
+	.arb_recovery = arb_lost_recovery,
 };
 
 static struct tegra_audio_platform_data tegra_audio_pdata[] = {
