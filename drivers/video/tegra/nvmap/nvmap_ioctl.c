30ab4d7479b6edf27495e701a095e6b3102cf5e4
Krishna Reddy <vdumpa@nvidia.com>
2011-08-30 10:15:44 -0700

    video: tegra: nvmap: Set page attributes as per request.
    After allocating pages, Set page attributes as per mem type
    requested.
    (cherry picked from commit e4862709dd7fb9799072576569fd532e0a597f31)
    
    Change-Id: If5e119366ffe6daaab78dc5915fa864d6932d464
    Reviewed-on: http://git-master/r/53828
    Reviewed-by: Vinod Rex <vrex@nvidia.com>
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>
    Tested-by: Krishna Reddy <vdumpa@nvidia.com>

--- drivers/video/tegra/nvmap/nvmap_ioctl.c
+++ drivers/video/tegra/nvmap/nvmap_ioctl.c
@@ -582,11 +582,8 @@ static int cache_maint(struct nvmap_client *client, struct nvmap_handle *h,
 	}
 
 	wmb();
-#if defined(CONFIG_ARCH_TEGRA_2x_SOC)
-	if (h->flags == NVMAP_HANDLE_WRITE_COMBINE)
-		goto out;
-#endif
-	if (h->flags == NVMAP_HANDLE_UNCACHEABLE || start == end)
+	if (h->flags == NVMAP_HANDLE_UNCACHEABLE ||
+	    h->flags == NVMAP_HANDLE_WRITE_COMBINE || start == end)
 		goto out;
 
 	if (fast_cache_maint(client, h, start, end, op))
