9f2693a80274bcd9eb8e7424bca87f34cc190741
Diwakar Tundlam <dtundlam@nvidia.com>
2011-09-02 10:38:24 -0700

    arm: tegra: power: set throttling temperature = 85C
    Earlier value of 75 had unnecessary double guardbanding.
    Changed 90C row in EDP table down to 85C to get throttling alert.
    
    Bug 862301
    
    Change-Id: Ie55d1257874aa336198f9965c867f5767864dc89
    Reviewed-on: http://git-master/r/50544
    Reviewed-by: Rohan Somvanshi <rsomvanshi@nvidia.com>
    Tested-by: Rohan Somvanshi <rsomvanshi@nvidia.com>

--- arch/arm/mach-tegra/board-cardhu-sensors.c
+++ arch/arm/mach-tegra/board-cardhu-sensors.c
@@ -554,7 +554,7 @@ static struct nct1008_platform_data cardhu_nct1008_pdata = {
 	.hysteresis = 5,
 	.shutdown_ext_limit = 90,
 	.shutdown_local_limit = 90,
-	.throttling_ext_limit = 75,
+	.throttling_ext_limit = 85,
 	.alarm_fn = tegra_throttling_enable,
 };
 
