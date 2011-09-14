32ff4babd2c8cf007cd19b623f01016a4e88e92a
Hiro Sugawara <hsugawara@nvidia.com>
2011-09-13 17:20:35 -0700

    arm: tegra: smmu: Remove IORESOURCE use from SMMU IOVA range
    SMMU simply needs to know its assigned IOVA range, but does not need
    address space resources.
    Bug 874438
    
    Merged in rel-12 branch with change ID I96a8718e692bbb96b5fce6ec7ebc90f6930f358c
    
    Change-Id: Iea4de33a46bd6eed19ccacb38ace9f02127f5137
    Signed-off-by: Mursalin Akon <makon@nvidia.com>
    Reviewed-on: http://git-master/r/52990
    Reviewed-by: Hiro Sugawara <hsugawara@nvidia.com>
    Reviewed-by: Allen Martin <amartin@nvidia.com>

--- arch/arm/mach-tegra/iovmm-smmu.c
+++ arch/arm/mach-tegra/iovmm-smmu.c
@@ -35,7 +35,7 @@
 #include <asm/cacheflush.h>
 #include <asm/page.h>
 #include <asm/cacheflush.h>
-
+#include "tegra_smmu.h"
 #include <mach/iovmm.h>
 #include <mach/iomap.h>
 
@@ -942,7 +942,8 @@ static struct tegra_iovmm_device_ops tegra_iovmm_smmu_ops = {
 static int smmu_probe(struct platform_device *pdev)
 {
 	struct smmu_device *smmu = NULL;
-	struct resource *regs = NULL, *window = NULL, *regs2 = NULL;
+	struct resource *regs = NULL, *regs2 = NULL;
+	struct tegra_smmu_window *window = NULL;
 	int e, asid;
 
 	if (!pdev) {
@@ -963,7 +964,7 @@ static int smmu_probe(struct platform_device *pdev)
 
 	regs = platform_get_resource_byname(pdev, IORESOURCE_MEM, "mc");
 	regs2 = platform_get_resource_byname(pdev, IORESOURCE_MEM, "ahbarb");
-	window = platform_get_resource_byname(pdev, IORESOURCE_MEM, "smmu");
+	window = tegra_smmu_window(0);
 
 	if (!regs || !regs2 || !window) {
 		pr_err(DRIVER_NAME ": No SMMU resources\n");
