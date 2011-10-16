b96dc093d8bd3efbc65cf7b0dfb7e13660ae0ad9
Alex Frid <afrid@nvidia.com>
2011-10-15 20:37:25 -0700

    sdhci: tegra: Fix clock rate selection logic
    Preserved clock rate target if it is below minimum (current code
    erroneously change rate target to maximum in this case). Changed
    standard clock rate to 104MHz (instead of 100MHz) to better match
    tegra dividers capabilities.
    
    Change-Id: I203974d064083d4ed49a7e196d86b8aa9a4b0837
    Reviewed-on: http://git-master/r/58272
    Tested-by: Aleksandr Frid <afrid@nvidia.com>
    Reviewed-by: Pavan Kunapuli <pkunapuli@nvidia.com>
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>

--- drivers/mmc/host/sdhci-tegra.c
+++ drivers/mmc/host/sdhci-tegra.c
@@ -37,7 +37,7 @@
 #define DRIVER_NAME    "sdhci-tegra"
 
 #define SDHCI_TEGRA_MIN_CONTROLLER_CLOCK	12000000
-#define SDHCI_TEGRA_STANDARD_CONTROLLER_CLOCK	100000000
+#define SDHCI_TEGRA_STANDARD_CONTROLLER_CLOCK	104000000
 #define SDHCI_TEGRA_MAX_CONTROLLER_CLOCK	208000000
 #define SDHCI_VENDOR_CLOCK_CNTRL       0x100
 
@@ -208,8 +208,8 @@ static void tegra_sdhci_set_clock(struct sdhci_host *sdhci, unsigned int clock)
 	if (clock > SDHCI_TEGRA_MIN_CONTROLLER_CLOCK &&
 			clock <= SDHCI_TEGRA_STANDARD_CONTROLLER_CLOCK)
 		clock = SDHCI_TEGRA_STANDARD_CONTROLLER_CLOCK;
-	else
-		clock = SDHCI_TEGRA_MAX_CONTROLLER_CLOCK;
+	else if (clock > SDHCI_TEGRA_STANDARD_CONTROLLER_CLOCK)
+			clock = SDHCI_TEGRA_MAX_CONTROLLER_CLOCK;
 	pr_debug("tegra sdhci clock %s %u enabled=%d\n",
 		mmc_hostname(sdhci->mmc), clock, host->clk_enabled);
 
