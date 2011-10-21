7ebaaea25400d3708b6f3ac3585b61b65ab99a17
Pradeep Goudagunta <pgoudagunta@nvidia.com>
2011-10-21 14:26:09 +0530

    serial: tegra: Avoid sleeping in atomic context
    Avoid mutex_lock to be called in spin_lock conetxt.
    
    Bug 890147
    
    Change-Id: Id1bdb348187b49abba4c44bcd09d5f74bb240042
    Reviewed-on: http://git-master/r/59545
    Tested-by: Pradeep Goudagunta <pgoudagunta@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- drivers/serial/tegra_hsuart.c
+++ drivers/serial/tegra_hsuart.c
@@ -632,11 +632,12 @@ static void tegra_uart_hw_deinit(struct tegra_uart_port *t)
 	/* Reset the Rx and Tx FIFOs */
 	tegra_fifo_reset(t, UART_FCR_CLEAR_XMIT | UART_FCR_CLEAR_RCVR);
 
-	clk_disable(t->clk);
 	t->baud = 0;
 	t->uart_state = TEGRA_UART_CLOSED;
 
 	spin_unlock_irqrestore(&t->uport.lock, flags);
+
+	clk_disable(t->clk);
 }
 
 static void tegra_uart_free_rx_dma(struct tegra_uart_port *t)
@@ -1495,6 +1496,7 @@ void tegra_uart_request_clock_off(struct uart_port *uport)
 {
 	unsigned long flags;
 	struct tegra_uart_port *t;
+	bool is_clk_disable = false;
 
 	if (IS_ERR_OR_NULL(uport))
 		BUG();
@@ -1504,10 +1506,14 @@ void tegra_uart_request_clock_off(struct uart_port *uport)
 	t = container_of(uport, struct tegra_uart_port, uport);
 	spin_lock_irqsave(&uport->lock, flags);
 	if (t->uart_state == TEGRA_UART_OPENED) {
-		clk_disable(t->clk);
+		is_clk_disable = true;
 		t->uart_state = TEGRA_UART_CLOCK_OFF;
 	}
 	spin_unlock_irqrestore(&uport->lock, flags);
+
+	if (is_clk_disable)
+		clk_disable(t->clk);
+
 	return;
 }
 
@@ -1516,6 +1522,7 @@ void tegra_uart_request_clock_on(struct uart_port *uport)
 {
 	unsigned long flags;
 	struct tegra_uart_port *t;
+	bool is_clk_enable = false;
 
 	if (IS_ERR_OR_NULL(uport))
 		BUG();
@@ -1523,10 +1530,14 @@ void tegra_uart_request_clock_on(struct uart_port *uport)
 	t = container_of(uport, struct tegra_uart_port, uport);
 	spin_lock_irqsave(&uport->lock, flags);
 	if (t->uart_state == TEGRA_UART_CLOCK_OFF) {
-		clk_enable(t->clk);
+		is_clk_enable = true;
 		t->uart_state = TEGRA_UART_OPENED;
 	}
 	spin_unlock_irqrestore(&uport->lock, flags);
+
+	if (is_clk_enable)
+		clk_enable(t->clk);
+
 	return;
 }
 
