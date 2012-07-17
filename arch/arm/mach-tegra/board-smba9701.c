b7ce7c2d0d6a57e5891056d1e360b2f22d060084
freexperia <jerpelea@gmail.com>
2012-07-17 22:09:47 +0300

    smba9701: panel init fix

--- arch/arm/mach-tegra/board-smba9701.c
+++ arch/arm/mach-tegra/board-smba9701.c
@@ -688,7 +688,7 @@ static void __init tegra_smba_init(void)
 	smba_spi_register_devices();
 
 	/* Register GPU devices */
-//	smba_gpu_register_devices();
+	smba_panel_init();
 
 	/* Register Audio devices */
 	smba_audio_register_devices();
