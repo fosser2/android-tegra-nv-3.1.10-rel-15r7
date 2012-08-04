cea9681b3b367cda9a3ed223c46a42efe1f174cf
Andrew Foss <fosser2@gmail.com>
2012-08-04 14:31:03 -0500

    smba1002: smba1002 cannot support higher i2c frequency

--- arch/arm/mach-tegra/board-smba1002-i2c.c
+++ arch/arm/mach-tegra/board-smba1002-i2c.c
@@ -34,7 +34,7 @@
 static struct tegra_i2c_platform_data smba_i2c1_platform_data = {
 	.adapter_nr	= 0,
 	.bus_count	= 1,
-	.bus_clk_rate	= { 400000, 0 },
+	.bus_clk_rate	= { 100000, 0 },
 };
 
 static const struct tegra_pingroup_config i2c2_ddc = {
@@ -59,13 +59,13 @@ static struct tegra_i2c_platform_data smba_i2c2_platform_data = {
 static struct tegra_i2c_platform_data smba_i2c3_platform_data = {
 	.adapter_nr     = 3,
 	.bus_count      = 1,
-	.bus_clk_rate   = { 400000, 0 },
+	.bus_clk_rate   = { 100000, 0 },
 };
 
 static struct tegra_i2c_platform_data smba_dvc_platform_data = {
 	.adapter_nr	= 4,
 	.bus_count	= 1,
-	.bus_clk_rate	= { 400000, 0 },
+	.bus_clk_rate	= { 100000, 0 },
 	.is_dvc		= true,
 };
 
