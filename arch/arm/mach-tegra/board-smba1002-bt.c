ff5d92b2ba2eff1919c14ab39e341caccc705312
Andrew Foss <fosser2@gmail.com>
2012-08-15 13:17:30 -0500

    smba1002: changes to Bluetooth as per ventana ([1] needs to be fixed)

--- arch/arm/mach-tegra/board-smba1002-bt.c
+++ arch/arm/mach-tegra/board-smba1002-bt.c
@@ -1,8 +1,6 @@
 /*
  * arch/arm/mach-tegra/board-smba1002-bt.c
  *
- * Copyright (C) 2011 Eduardo José Tagle <ejtagle@tutopia.com>
- *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
  * may be copied, distributed, and modified under those terms.
@@ -44,80 +42,63 @@
 #include "gpio-names.h"
 #include "devices.h"
 
-static struct resource smba_bcm4329_rfkill_resources[] = {
+static struct resource ventana_bcm4329_rfkill_resources[] = {
 	{
-		.name   = "bcm4329_nreset_gpio",
+		.name   = "bcm4329_nshutdown_gpio",
 		.start  = SMBA1002_BT_RESET,
 		.end    = SMBA1002_BT_RESET,
 		.flags  = IORESOURCE_IO,
 	},
 };
 
-static struct platform_device smba_bcm4329_rfkill_device = {
+static struct platform_device ventana_bcm4329_rfkill_device = {
 	.name = "bcm4329_rfkill",
 	.id             = -1,
-	.num_resources  = ARRAY_SIZE(smba_bcm4329_rfkill_resources),
+	.num_resources  = ARRAY_SIZE(ventana_bcm4329_rfkill_resources),
 	.resource       = smba_bcm4329_rfkill_resources,
 };
 
-void __init smba_bt_rfkill(void)
+static void __init smba_bt_rfkill(void)
 {
 	/*Add Clock Resource*/
-	int res = clk_add_alias("bcm4329_32k_clk", smba_bcm4329_rfkill_device.name, \
+	clk_add_alias("bcm4329_32k_clk", smba_bcm4329_rfkill_device.name, \
 				"blink", NULL);
-	printk("Initializing BT RFKILL, clk_add_alias: %d \n", res);
-	res = platform_device_register(&smba_bcm4329_rfkill_device);
-	if (res)
-		printk("Error on BT RFKILL reg, result: %d \n", res);
 	return;
 }
 
-void __init smba_setup_bluesleep(void)
+static struct resource ventana_bluesleep_resources[] = {
+	[0] = {
+		.name = "gpio_host_wake",
+			.start  = SMBA1002_BT_IRQ,
+			.end    = SMBA1002_BT_IRQ,
+			.flags  = IORESOURCE_IO,
+	},
+	//FIX THIS SECTION
+	[1] = {
+		.name = "gpio_ext_wake",
+			.start  = TEGRA_GPIO_PU1,
+			.end    = TEGRA_GPIO_PU1,
+			.flags  = IORESOURCE_IO,
+	},
+	[2] = {
+		.name = "host_wake",
+			.start  = TEGRA_GPIO_TO_IRQ(SMBA1002_BT_IRQ),
+			.end    = TEGRA_GPIO_TO_IRQ(SMBA1002_BT_IRQ),
+			.flags  = IORESOURCE_IRQ | IORESOURCE_IRQ_HIGHEDGE,
+	},
+};
+
+static struct platform_device ventana_bluesleep_device = {
+	.name           = "bluesleep",
+	.id             = -1,
+	.num_resources  = ARRAY_SIZE(smba_bluesleep_resources),
+	.resource       = smba_bluesleep_resources,
+};
+
+static void __init smba_setup_bluesleep(void)
 {
-	struct platform_device *pdev = NULL;
-	struct resource *res;
-
-	pdev = platform_device_alloc("bluesleep", 0);
-	if (!pdev) {
-		pr_err("unable to allocate platform device for bluesleep");
-		return;
-	}
-
-	res = kzalloc(sizeof(struct resource) * 2, GFP_KERNEL);
-	if (!res) {
-		pr_err("unable to allocate resource for bluesleep\n");
-		goto err_free_dev;
-	}
-
-	res[0].name   = "gpio_host_wake";
-	res[0].start  = SMBA1002_BT_IRQ;
-	res[0].end    = SMBA1002_BT_IRQ;
-	res[0].flags  = IORESOURCE_IO;
-
-	res[1].name   = "host_wake";
-	res[1].start  = gpio_to_irq(SMBA1002_BT_IRQ);
-	res[1].end    = gpio_to_irq(SMBA1002_BT_IRQ);
-	res[1].flags  = IORESOURCE_IRQ | IORESOURCE_IRQ_LOWEDGE;
-
-	if (platform_device_add_resources(pdev, res, 2)) {
-		pr_err("unable to add resources to bluesleep device\n");
-		goto err_free_res;
-	}
-
-	if (platform_device_add(pdev)) {
-		pr_err("unable to add bluesleep device\n");
-		goto err_free_res;
-	}
-
+	platform_device_register(&smba_bluesleep_device);
 	tegra_gpio_enable(SMBA1002_BT_IRQ);
-
-	kfree(res);
-
-	return;
-
-err_free_res:
-	kfree(res);
-err_free_dev:
-	platform_device_put(pdev);
+	tegra_gpio_enable(SMBA1002_BT_RESET);
 	return;
 }
