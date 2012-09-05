9ca40532bf65715840ebbf73da5a33c0c3f802cf
Andrew Foss <fosser2@gmail.com>
2012-09-04 23:32:05 -0500

    smba1002: clock changes to match ventana setup

--- arch/arm/mach-tegra/board-smba1002-clocks.c
+++ arch/arm/mach-tegra/board-smba1002-clocks.c
@@ -266,26 +266,14 @@ static __initdata struct tegra_clk_init_table smba_clk_init_table[] = {
 
 static __initdata struct tegra_clk_init_table smba_clk_init_table[] = {
 	/* name			parent		 rate		enabled */
-	{ "cdev1",		NULL,		     0, true },
-	{ "i2s1",		"pll_a_out0", 	     0,	false},
-	{ "i2s2",		"pll_a_out0", 	     0,	false},
-	{ "spdif_out",	"pll_a_out0",  	     0,	false},
-	{ "sdmmc1",		"clk_m",	  48000000,	true },
-	{ "sdmmc2",		"clk_m",	  48000000,	true },
-	{ "sdmmc4",		"clk_m",	  48000000,	true },
-	{ "ndflash",	"pll_p",	 108000000,	true },
-	{ "pwm",		"clk_32k",		 32768,	false},	
-	{ "usbd",		"clk_m",	  12000000,	true },		/* fsl-tegra-udc , utmip-pad , tegra_ehci.0 , tegra_otg - we need this to be always on to always get hotplug events */
-	{ "usb2",		"clk_m",	  12000000,	false},		/* tegra_ehci.1 - Really unused*/
-	{ "usb3",		"clk_m",	  12000000,	true },		/* tegra_ehci.2 - we need this to be always on to always get hotplug events */
-	{ "i2c1",		"clk_m",	    800000,	false},		/* tegra-i2c.0 */
-	{ "i2c2",		"clk_m",	    315789,	false},		/* tegra-i2c.1 */
-	{ "i2c3",		"clk_m",	    800000,	false},		/* tegra-i2c.2 */
-	{ "dvc",		"clk_m",	   2400000,	false},		/* tegra-i2c.3 */
-	//{ "pll_p_out4",	"pll_p",	24000000,	true },
-	{ "blink",	"clk_32k",	32768,		false},
-	
-	{ NULL,			NULL,		         0,		0},
+	{ "cdev1",		NULL,		     0, 	true },
+	{ "blink",	"clk_32k",		 32768,		false},
+	{ "pll_p_out4",	"pll_p",  24000000,		true },
+	{ "pwm",	"clk_32k",		 32768,		false},
+	{ "i2s1",	"pll_a_out0",		 0,		false},
+	{ "i2s2",	"pll_a_out0",		 0,		false},
+	{ "spdif_out",	"pll_a_out0",	 0,		false},	
+	{ NULL,			NULL,		     0,			0},
 }; 
 
 void __init smba_clks_init(void)
