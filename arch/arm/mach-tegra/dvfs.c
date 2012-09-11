df4dfd041f8342384b79611a6504eacd28f6b46e
Clemente <clemsyn@yahoo.com>
2012-09-11 08:26:29 -0500

    Update arch/arm/mach-tegra/dvfs.c
    undervolting code

--- arch/arm/mach-tegra/dvfs.c
+++ arch/arm/mach-tegra/dvfs.c
@@ -50,6 +50,11 @@ static DEFINE_MUTEX(rail_disable_lock);
 
 static int dvfs_rail_update(struct dvfs_rail *rail);
 
+#ifdef CONFIG_DRH_OC_MODE
+struct dvfs *cpu_dvfs = NULL;
+extern int *UV_mV_Ptr;
+#endif
+
 void tegra_dvfs_add_relationships(struct dvfs_relationship *rels, int n)
 {
 	int i;
@@ -354,7 +359,20 @@ __tegra_dvfs_set_rate(struct dvfs *d, unsigned long rate)
 				" %s\n", d->millivolts[i], d->clk_name);
 			return -EINVAL;
 		}
+
+#ifdef CONFIG_DRH_OC_MODE
+
+                if(UV_mV_Ptr != NULL)
+                        d->cur_millivolts = d->millivolts[i] - UV_mV_Ptr[i];
+                else
+                        d->cur_millivolts = d->millivolts[i];
+
+                //printk( "d->clk_name = %s || ", d->clk_name );
+                
+#else
 		d->cur_millivolts = d->millivolts[i];
+#endif
+
 	}
 
 	d->cur_rate = rate;
@@ -467,6 +485,18 @@ int __init tegra_enable_dvfs_on_clk(struct clk *c, struct dvfs *d)
 	}
 
 	c->dvfs = d;
+
+#ifdef CONFIG_DRH_OC_MODE
+        
+        if(cpu_dvfs == NULL)
+        {
+                if(strcmp(d->clk_name, "cpu")==0)
+                {
+                             printk( "TEGRA_OC: CPU DVFS FOUND" );
+                             cpu_dvfs = d;
+                }
+          }
+#endif
 
 	mutex_lock(&dvfs_lock);
 	list_add_tail(&d->reg_node, &d->dvfs_rail->dvfs);
