bdadb3fb68637f1227770ab7f4ecac6a83b95edb
Andrew Foss <fosser2@gmail.com>
2012-10-31 12:10:45 -0500

    smba1002: GPU memory changes

--- arch/arm/mach-tegra/board-smba1002.h
+++ arch/arm/mach-tegra/board-smba1002.h
@@ -87,12 +87,11 @@
 #define SMBA1002_MEM_SIZE 			SZ_512M			/* Total memory */
 #define SMBA1002_MEM_BANKS			1
 
-//#define SMBA1002_GPU_MEM_SIZE 		SZ_128M 		/* Memory reserved for GPU */
-#define SMBA1002_GPU_MEM_SIZE   (80*SZ_1M)
+#define SMBA1002_GPU_MEM_SIZE  			SZ_128M			/* Memory reserved for GPU */
 
-#define SMBA1002_FB1_MEM_SIZE 	(5*SZ_1M)			/* Memory reserved for Framebuffer 1: LCD */
-#define SMBA1002_FB2_MEM_SIZE 	(4*SZ_1M)		/* Memory reserved for Framebuffer 2: HDMI out */
-
+#define SMBA1002_FB1_MEM_SIZE 	SZ_8M			/* Memory reserved for Framebuffer 1: LCD */
+#define SMBA1002_FB2_MEM_SIZE 	SZ_16M			/* Memory reserved for Framebuffer 2: HDMI out */
+	
 #define DYNAMIC_GPU_MEM 1							/* use dynamic memory for GPU */
 
 //#define SMBA1002_GPS  							/* Define for Fosser's GPS MOD */
