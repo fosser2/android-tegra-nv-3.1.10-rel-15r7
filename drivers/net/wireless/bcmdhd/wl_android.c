22f80606afa57ca780a14b021d730e45b8cbd48c
Andrew Foss <fosser2@gmail.com>
2012-12-27 10:52:07 -0600

    net: remove smba hack as it is not needed (wifi working without hack)

--- drivers/net/wireless/bcmdhd/wl_android.c
+++ drivers/net/wireless/bcmdhd/wl_android.c
@@ -374,9 +374,7 @@ static int wl_android_get_p2p_dev_addr(struct net_device *ndev, char *command, i
 int wl_android_wifi_on(struct net_device *dev)
 {
 	int ret = 0;
-	int retry = 1; //is this needed?
 	
-onretry:
 	printk("%s in\n", __FUNCTION__);
 	if (!dev) {
 		DHD_ERROR(("%s: dev is null\n", __FUNCTION__));
@@ -397,15 +395,7 @@ onretry:
 	}
 	dhd_net_if_unlock(dev);
 	
-////////////////////////////////// SMBA Patch /////////////////////////////////////
-	if (ret == -EIO && retry < 5) {
-		wl_android_wifi_off(dev);
-		msleep(1000);
-		printk("I/O Error on wifi, power down and try again. Attempt %d/5\n", retry);
-		goto onretry;
-	}
-////////////////////////////////////////////////////////////////////////////////////
-	
+
 	return ret;
 }
 
