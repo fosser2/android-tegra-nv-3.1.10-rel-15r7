9cec839f89422993276c64c4d682dd3b7d0ce2a3
fosser2 <fosser2@gmail.com>
2013-01-09 14:56:56 -0600

    mem: Restore asynchronous compaction support in lowmemorykiller. from intersectRaven https://github.com/intersectRaven/nexus7-kernel/commit/ed6c4b7b1384cb06eee7feb71eff995cf020739e

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -41,6 +41,7 @@
 #include <linux/device.h>
 #include <linux/err.h>
 #include <linux/mm_inline.h>
+#include <linux/compaction.h>
 
 static uint32_t lowmem_debug_level = 2;
 static int lowmem_adj[6] = {
@@ -89,6 +90,8 @@ enum pageout_io {
 #endif /* CONFIG_ZRAM_FOR_ANDROID */
 static struct task_struct *lowmem_deathpending;
 static unsigned long lowmem_deathpending_timeout;
+
+extern int compact_nodes(bool sync);
 
 #define lowmem_print(level, x...)			\
 	do {						\
@@ -213,6 +216,8 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	lowmem_print(4, "lowmem_shrink %lu, %x, return %d\n",
 		     sc->nr_to_scan, sc->gfp_mask, rem);
 	read_unlock(&tasklist_lock);
+	if (selected)
+		compact_nodes(false);
 	return rem;
 }
 
