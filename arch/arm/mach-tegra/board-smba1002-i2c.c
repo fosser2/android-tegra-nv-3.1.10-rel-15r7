baca184c42ea4d9913d20920b861ab0911bb9595
fosser2 <fosser2@teamdrh.com>
2013-02-27 15:57:15 -0600

    smba1002: bumped harmony i2c back to 100000

--- arch/arm/mach-tegra/board-smba1002-i2c.c
+++ arch/arm/mach-tegra/board-smba1002-i2c.c
@@ -50,10 +50,10 @@ static const struct tegra_pingroup_config i2c2_gen2 = {
 static struct tegra_i2c_platform_data smba_i2c2_platform_data = {
 	.adapter_nr	= 1,
 	.bus_count	= 2,
-	.bus_clk_rate	= { 10000, 10000 },
+	.bus_clk_rate	= { 100000, 100000 },
 	//FIX ME
-	//.bus_mux	= { &i2c2_ddc, &i2c2_gen2 },
-	.bus_mux	= { &i2c2_ddc },
+	.bus_mux	= { &i2c2_ddc, &i2c2_gen2 },
+	//.bus_mux	= { &i2c2_ddc },
 	.bus_mux_len	= { 1, 1 },
 };
 
