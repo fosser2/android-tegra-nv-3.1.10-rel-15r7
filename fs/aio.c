d8bf2c40723dbf0964d6e2bc98d69d6868bb4e7b
theXfactor2011 <Harry.Anuszewski@gmail.com>
2013-03-09 14:20:50 -0500

    Asynchronous I/O to solid-state disk greatly increased. 34% improvement.

--- fs/aio.c
+++ fs/aio.c
@@ -1622,7 +1622,7 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 	struct kioctx *ctx;
 	long ret = 0;
 	int i;
-	struct blk_plug plug;
+	//struct blk_plug plug;
 
 	if (unlikely(nr < 0))
 		return -EINVAL;
@@ -1639,7 +1639,7 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 		return -EINVAL;
 	}
 
-	blk_start_plug(&plug);
+	//blk_start_plug(&plug);
 
 	/*
 	 * AKPM: should this return a partial result if some of the IOs were
@@ -1663,7 +1663,7 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 		if (ret)
 			break;
 	}
-	blk_finish_plug(&plug);
+	//blk_finish_plug(&plug);
 
 	put_ioctx(ctx);
 	return i ? i : ret;
