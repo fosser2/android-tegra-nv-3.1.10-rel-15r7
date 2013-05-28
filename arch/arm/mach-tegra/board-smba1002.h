eb6f3865a59ad2db61eb402101476e616318f5f0
Clemente <clemsyn@yahoo.com>
2013-05-27 19:15:52 -0500

    Decrease memory of fb1 to 8M

--- arch/arm/mach-tegra/board-smba1002.h
+++ arch/arm/mach-tegra/board-smba1002.h
@@ -80,7 +80,7 @@
 
 #define SMBA1002_GPU_MEM_SIZE  			(SZ_1M*128)			/* Memory reserved for GPU */
 
-#define SMBA1002_FB1_MEM_SIZE 	SZ_16M			/* Memory reserved for Framebuffer 1: LCD */
+#define SMBA1002_FB1_MEM_SIZE 	SZ_8M			/* Memory reserved for Framebuffer 1: LCD */
 #define SMBA1002_FB2_MEM_SIZE 	SZ_8M			/* Memory reserved for Framebuffer 2: HDMI out */
 	
 #define DYNAMIC_GPU_MEM 0							/* use dynamic memory for GPU */
