294f35e6997cc3a8e5cf1969f4e9d164f8732ab4
Boris Popov <bvpopov@gmail.com>
2014-03-07 03:59:33 -0800

    board-smba1002-power: Fix suspend_data

--- arch/arm/mach-tegra/board-smba1002-power.c
+++ arch/arm/mach-tegra/board-smba1002-power.c
@@ -331,11 +331,11 @@ static struct tegra_suspend_platform_data smba_suspend_data = {
          * Check power on time and crystal oscillator start time
          * for appropriate settings.
          */
-        .cpu_timer      = 5000,
-        .cpu_off_timer  = 5000,
-        .suspend_mode   = TEGRA_SUSPEND_LP0,
+        .cpu_timer      = 2000,
+        .cpu_off_timer  = 100,
+        .suspend_mode   = TEGRA_SUSPEND_LP1,
         .core_timer     = 0x7e7e,
-        .core_off_timer = 0x7f,
+        .core_off_timer = 0xf,
         .corereq_high   = false,
         .sysclkreq_high = true,
         .board_suspend = smba_board_suspend,
